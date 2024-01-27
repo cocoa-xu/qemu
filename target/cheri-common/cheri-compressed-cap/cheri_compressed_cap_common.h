1b030f4af94f272230c79c3da8c07eb57e7dff0c
Cocoa <i@uwucocoa.moe>
2024-01-27 15:36:42 +0700

    fixed a typo

--- target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
+++ target/cheri-common/cheri-compressed-cap/cheri_compressed_cap_common.h
@@ -174,7 +174,7 @@ static inline bool _cc_N(exactly_equal)(const _cc_cap_t* a, const _cc_cap_t* b) 
 }
 
 static inline bool _cc_N(raw_equal)(const _cc_cap_t* a, const _cc_cap_t* b) {
-    return a->_cr_cursor == b->_cr_cursor && a->cr_pesbt == b->cr_pesbt && _ccN(test_cc_length_equal)(a->_cr_top, b->_cr_top) &&
+    return a->_cr_cursor == b->_cr_cursor && a->cr_pesbt == b->cr_pesbt && _cc_N(test_cc_length_equal)(a->_cr_top, b->_cr_top) &&
            a->cr_base == b->cr_base && a->cr_tag == b->cr_tag && a->cr_bounds_valid == b->cr_bounds_valid &&
            a->cr_exp == b->cr_exp && a->cr_extra == b->cr_extra;
 }
